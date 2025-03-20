b36169fd2ffb24ac682bc6b374200e47bbd3a25a
Alexander Zaykov <64609206+ZaykoFF@users.noreply.github.com>
2025-03-20 15:29:02 +0300

    fix

--- template-parts/block-chess.php
+++ template-parts/block-chess.php
@@ -13,7 +13,7 @@
     </div>
     <div class="chess-bottom
         <? if ($slug == 'kommunar' or $slug == 'dinastia') echo ' chess-bottom-kommunar'; ?>" 
-        <? if ($slug == 'kommunar' or $slug == 'dinastia') echo 'style="background-image:url(/wp-content/themes/su-7/img/house_min.svg);"'; ?>>
+        <? if ($slug == 'kommunar' or $slug == 'dinastia') echo 'style="background-image:url(/wp-content/themes/su-7/img/house_min.svg);"'; ?>
         <? if ($slug == 'bit') echo 'style="background-image:url(/wp-content/themes/su-7/img/house_bit.svg);"'; ?>>
         <div class="chess-bottom-top">
             <?php get_template_part('template-parts/block', 'chessSwitch'); ?>
