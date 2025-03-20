e4929c07ead94d1285e3ecac9456846ddf1eea09
Alexander Zaykov <64609206+ZaykoFF@users.noreply.github.com>
2025-03-20 15:27:05 +0300

    house plan svg for bit

--- template-parts/block-chess.php
+++ template-parts/block-chess.php
@@ -11,7 +11,10 @@
     <div class="chess-top">
         <h1>Выбрать квартиру</h1> 
     </div>
-    <div class="chess-bottom<? if ($slug == 'kommunar' or $slug == 'dinastia') echo ' chess-bottom-kommunar'; ?>" <? if ($slug == 'kommunar' or $slug == 'dinastia') echo 'style="background-image:url(/wp-content/themes/su-7/img/house_min.svg);"'; ?>>
+    <div class="chess-bottom
+        <? if ($slug == 'kommunar' or $slug == 'dinastia') echo ' chess-bottom-kommunar'; ?>" 
+        <? if ($slug == 'kommunar' or $slug == 'dinastia') echo 'style="background-image:url(/wp-content/themes/su-7/img/house_min.svg);"'; ?>>
+        <? if ($slug == 'bit') echo 'style="background-image:url(/wp-content/themes/su-7/img/house_bit.svg);"'; ?>>
         <div class="chess-bottom-top">
             <?php get_template_part('template-parts/block', 'chessSwitch'); ?>
         </div>
